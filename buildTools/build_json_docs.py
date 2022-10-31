f46df9a1c48940ffe09ed24a5e3574a234877e3e
ajh123 <41990982+ajh123@users.noreply.github.com>
2022-10-31 10:22:28 +0000

    Nearly working getting retuns in json docs

--- buildTools/build_json_docs.py
+++ buildTools/build_json_docs.py
@@ -45,6 +45,12 @@ for name in projects:
                 redefining = False
                 startOfFunc = False
                 inFunc = False
+                returns = ""
+                prev_returns = ""
+                extra_returns = ""
+                indentifier_count = 0
+                indentifier_threshold = 2
+                is_generic = False
                 for token in tokens:
                     doc = token.javadoc
                     if not isInClass and isinstance(token, javalang.tokenizer.Modifier):
@@ -54,29 +60,55 @@ for name in projects:
                         isReadyForClass = False
                     if isInClass:
                         if isinstance(token, javalang.tokenizer.Modifier):
-                            if not inFunc:
-                                gotName = False
-                                l, c = token.position
-
-                                prevFuncLine = funcLine
-                                funcLine = l
-                                if not startOfFunc:
-                                    startOfFunc = True
-                                modifiers.append(token.value)
-                                
-                                print(lines[funcLine-1])
-                                
-                                if "{" in lines[funcLine-1]:
-                                    inFunc = True
-                                if "=" in lines[funcLine-1] and inFunc:
-                                    redefining = True
-                        if not gotName and isinstance(token, javalang.tokenizer.Identifier):
+                            gotName = False
                             l, c = token.position
+
+                            prevFuncLine = funcLine
+                            funcLine = l
+                            if not startOfFunc:
+                                startOfFunc = True
+                                indentifier_count = 0
+                            modifiers.append(token.value)
+
+                            if "{" in lines[funcLine-1]:
+                                inFunc = True
+                            if "=" in lines[funcLine-1] and inFunc:
+                                redefining = True
+                        is_primative = False
+                        l, c = token.position
+                        if token.value == "void":
+                            is_primative = True
+                        if token.value == "<":
+                            is_generic = True
+                            indentifier_count -= 1
+                        if token.value == ">":
+                            is_generic = False
+
+                        if isinstance(token, javalang.tokenizer.Identifier) and inFunc and l == funcLine and not gotName:
+                            if "<" in lines[l-1] and ">" in lines[l-1] and token.value in lines[l-1]:
+                                if not is_generic:
+                                    extra_returns += token.value
+                                    indentifier_threshold += 1
+                                    indentifier_count += 1
+                                else:
+                                    extra_returns += "<"+token.value+">"
+                                    indentifier_threshold += 1
+                                    indentifier_count += 1
+                                print(extra_returns)
+
+                        if isinstance(token, javalang.tokenizer.Identifier) or is_primative and not is_generic:
                             if "{" in lines[l-1] and not "=" in lines[l-1]:
-                                if l == funcLine:
+                                indentifier_count += 1
+
+                                if returns == "":
+                                    returns = token.value + extra_returns
+                                    prev_returns = returns
+                                    gotName = False
+
+                                if indentifier_count == indentifier_threshold and not gotName and l == funcLine:
                                     gotName = True
                                     name = token.value
-                                    print(name, redefining)
+
                                     if not redefining and inFunc:
                                         try:
                                             docBlock: javalang.javadoc.DocBlock = javalang.javadoc.parse(prevDoc)
@@ -84,22 +116,26 @@ for name in projects:
                                                 "desc": docBlock.description,
                                                 "modifiers": modifiers.copy(),
                                                 "line": funcLine,
-                                                "redf": redefining
+                                                "returns": prev_returns
                                             }
                                         except ValueError:
                                             out[name] = {
                                                 "desc": "",
                                                 "modifiers": modifiers.copy(),
                                                 "line": funcLine,
-                                                "redf": redefining
+                                                "returns": prev_returns
                                             }
 
                                         prevDoc = ""
                                         startOfFunc = False
                                         inFunc = False
+                                        returns = ""
+                                        indentifier_threshold = 2
+                                        extra_returns = ""
                             modifiers.clear()
                             redefining = False
                         if doc is not None:
                             prevDoc = doc
+
 
 print(out)
