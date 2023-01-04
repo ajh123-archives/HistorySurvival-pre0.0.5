3dd1778738c0a0f52192e781be578edf044b3c1d
ajh123 <ajh123@users.noreply.github.com>
2023-01-04 18:12:37 +0000

    Screenify

--- game/core/src/tk/minersonline/history_survival/HistorySurvival.java
+++ game/core/src/tk/minersonline/history_survival/HistorySurvival.java
@@ -1,62 +1,29 @@
 package tk.minersonline.history_survival;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.ModelBatch;
-import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
-import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tk.minersonline.history_survival.net.Packet;
-import tk.minersonline.history_survival.voxels.PerlinNoiseGenerator;
-import tk.minersonline.history_survival.voxels.VoxelWorld;
+import tk.minersonline.history_survival.screens.GameScreen;
 
 import java.io.IOException;
 
-public class HistorySurvival extends ApplicationAdapter {
+public class HistorySurvival extends Game {
 	private static final Logger logger = LoggerFactory.getLogger("HistorySurvival");
-	SpriteBatch spriteBatch;
-	BitmapFont font;
-	ModelBatch modelBatch;
-	PerspectiveCamera camera;
-	Environment lights;
-	FirstPersonCameraController controller;
-	VoxelWorld voxelWorld;
+	public SpriteBatch spriteBatch;
+	public BitmapFont font;
 
 	@Override
 	public void create () {
 		spriteBatch = new SpriteBatch();
 		font = new BitmapFont();
-		modelBatch = new ModelBatch();
-		DefaultShader.defaultCullFace = GL20.GL_FRONT;
-		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		camera.near = 0.5f;
-		camera.far = 1000;
-		controller = new FirstPersonCameraController(camera);
-		Gdx.input.setInputProcessor(controller);
 
-		lights = new Environment();
-		lights.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1.f));
-		lights.add(new DirectionalLight().set(1, 1, 1, 0, -1, 0));
+		this.setScreen(new GameScreen(this));
 
-		MathUtils.random.setSeed(0);
-		voxelWorld = new VoxelWorld(20, 4, 20);
-		PerlinNoiseGenerator.generateVoxels(voxelWorld, 0, 63, 10);
-		float camX = voxelWorld.voxelsX / 2f;
-		float camZ = voxelWorld.voxelsZ / 2f;
-		float camY = voxelWorld.getHighest(camX, camZ) + 1.5f;
-		camera.position.set(camX, camY, camZ);
 		logger.info("Game started");
 
 
@@ -75,23 +42,12 @@ public class HistorySurvival extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0.4f, 0.4f, 0.4f, 1f, true);
-		modelBatch.begin(camera);
-		modelBatch.render(voxelWorld, lights);
-		modelBatch.end();
-		controller.update();
-
-		spriteBatch.begin();
-		font.draw(spriteBatch, "fps: " + Gdx.graphics.getFramesPerSecond() + ", #visible chunks: " + voxelWorld.renderedChunks + "/"
-				+ voxelWorld.numChunks, 0, 20);
-		spriteBatch.end();
+		super.render();
 	}
 
 	@Override
-	public void resize (int width, int height) {
-		spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
-		camera.viewportWidth = width;
-		camera.viewportHeight = height;
-		camera.update();
+	public void dispose() {
+		spriteBatch.dispose();
+		font.dispose();
 	}
 }
---
+++ game/core/src/tk/minersonline/history_survival/screens/GameScreen.java
@@ -0,0 +1,100 @@
+package tk.minersonline.history_survival.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
+import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.ScreenUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tk.minersonline.history_survival.HistorySurvival;
+import tk.minersonline.history_survival.voxels.PerlinNoiseGenerator;
+import tk.minersonline.history_survival.voxels.VoxelWorld;
+
+public class GameScreen implements Screen {
+	private static final Logger logger = LoggerFactory.getLogger("HistorySurvival");
+	HistorySurvival game;
+	ModelBatch modelBatch;
+	PerspectiveCamera camera;
+	Environment lights;
+	FirstPersonCameraController controller;
+	VoxelWorld voxelWorld;
+
+	public GameScreen(HistorySurvival game) {
+		this.game = game;
+	}
+
+	@Override
+	public void show() {
+		modelBatch = new ModelBatch();
+		DefaultShader.defaultCullFace = GL20.GL_FRONT;
+		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.near = 0.5f;
+		camera.far = 1000;
+		controller = new FirstPersonCameraController(camera);
+		Gdx.input.setInputProcessor(controller);
+
+		lights = new Environment();
+		lights.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1.f));
+		lights.add(new DirectionalLight().set(1, 1, 1, 0, -1, 0));
+
+		MathUtils.random.setSeed(0);
+		voxelWorld = new VoxelWorld(20, 4, 20);
+		PerlinNoiseGenerator.generateVoxels(voxelWorld, 0, 63, 10);
+		float camX = voxelWorld.voxelsX / 2f;
+		float camZ = voxelWorld.voxelsZ / 2f;
+		float camY = voxelWorld.getHighest(camX, camZ) + 1.5f;
+		camera.position.set(camX, camY, camZ);
+	}
+
+	@Override
+	public void render(float delta) {
+		ScreenUtils.clear(0.4f, 0.4f, 0.4f, 1f, true);
+		modelBatch.begin(camera);
+		modelBatch.render(voxelWorld, lights);
+		modelBatch.end();
+		controller.update();
+
+		game.spriteBatch.begin();
+		game.font.draw(game.spriteBatch, "fps: " + Gdx.graphics.getFramesPerSecond() + ", #visible chunks: " + voxelWorld.renderedChunks + "/"
+				+ voxelWorld.numChunks, 0, 20);
+		game.spriteBatch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		game.spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+		camera.viewportWidth = width;
+		camera.viewportHeight = height;
+		camera.update();
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
